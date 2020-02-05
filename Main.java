e8116ae2ffe22383fe7a38bc231db3a7a6b1286b
Mgurung3032 <60681894+Mgurung3032@users.noreply.github.com>
2020-02-04 23:05:50 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,95 @@
+//Manisha Gurung
+//02/03/20 COSC 251
+//Assignment 1
+// Team partner : Dashawn
+package com.application;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("************");
+
+	System.out.println("*          *");
+
+	System.out.println("*          *");
+
+	System.out.println("*          *");
+
+	System.out.println("*          *");
+
+	System.out.println("*          *");
+
+	System.out.println("*          *");
+
+	System.out.println("*          *");
+
+	System.out.println("************");
+
+
+	System.out.println("      ***");
+		System.out.println("   *        *  ");
+		System.out.println(" *            * ");
+		System.out.println("*              *");
+		System.out.println("*              *");
+		System.out.println("*              *");
+		System.out.println(" *           *  ");
+		System.out.println("   *       *   ");
+		System.out.println("      ***");
+
+		System.out.println("  *  ");
+		System.out.println(" *** ");
+		System.out.println("*****");
+		System.out.println("  *  ");
+		System.out.println("  *  ");
+		System.out.println("  *  ");
+		System.out.println("  *  ");
+		System.out.println("  *  ");
+		System.out.println("  *  ");
+
+		System.out.println("     *    ");
+		System.out.println("    * *  ");
+		System.out.println("  *     *  ");
+		System.out.println(" *       *");
+		System.out.println("*         *");
+		System.out.println(" *       *");
+		System.out.println("  *     *  ");
+		System.out.println("    * *  ");
+		System.out.println("     *    ");
+
+
+		Scanner userInput = new Scanner(System.in);
+
+		int b = 0;
+
+		int c = 0;
+
+		int a = b * c;
+
+		//This program performs a sample payroll calculation
+
+		System.out.println("Enter an integer:");
+		int x = userInput.nextInt();
+		System.out.println("Enter the second number:");
+
+		int y = userInput.nextInt();
+
+		int Sum = x + y;
+
+		int multiply = x * y;
+
+		int subtract = x - y;
+
+		int Quotient = x / y;
+
+		System.out.println("Sum: " + Sum);
+
+		System.out.println("multiply: " + multiply);
+
+		System.out.println("subtract: " + subtract);
+
+		System.out.println("quotient: " + Quotient);
+
+
+    }
+}
